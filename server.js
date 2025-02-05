b456748971a71a9e1afde1e74f27ecb466457349
Rizal Suhaimi <105201048+incursio16@users.noreply.github.com>
2025-02-05 21:35:08 +0800

    simple back-end test

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 4001;
 
+app.use((req, res, next) => {
+    console.log(`This appears before the request is passed to the bodyParser.json() method. req.body lookes like:`)
+    console.log(req.body)
+    next()
+});
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ info: 'Node.js, Express, and Postgress API'})
+});
+
+app.post('/', (req, res) => {
+    console.log(`After the request is passed to the bodyParser.json() method, the req.body lookes like:`)
+    console.log(req.body)
+    res.status(200).json(req.body)
+});
+
 app.listen(PORT, () => {
     console.log(`Server is listening to port ${PORT}`)
 });
-
-module.exports = app;
